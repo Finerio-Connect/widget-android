7a1393e8fd692daaf46ac202fcf4a1e8d0b6901b
Irving Lara <>
2021-05-19 12:21:20 -0500

    feedback widget

--- account/src/main/java/com/finerioconnect/widget/utils/SessionFieldList.java
+++ account/src/main/java/com/finerioconnect/widget/utils/SessionFieldList.java
@@ -5,13 +5,11 @@ import com.finerioconnect.widget.remote.data.Field;
 import java.util.List;
 
 public class SessionFieldList {
-    private List<Field> fieldList;
+    private final List<Field> fieldList;
     public List<Field> getFieldList() { return fieldList; }
-    public void setFieldList(List<Field> fieldList) { this.fieldList = fieldList; }
 
-    private int bankId;
+    private final int bankId;
     public int getBankId() { return bankId; }
-    public void setBankId(int bankId) { this.bankId = bankId; }
 
     public SessionFieldList(int bankId, List<Field> fieldList) {
         this.fieldList = fieldList;
